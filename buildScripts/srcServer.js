9a8e673f13e074133d6d8018031d6ffab7739afc
Johan Gustafsson <johanrg70@gmail.com>
2016-11-14 15:56:44 +0100

    More testing

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+    }
+});
